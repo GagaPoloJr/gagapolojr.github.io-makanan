92aec054d7f5546cccce5a95770015c5e9fcec05
GagaPoloJr <rahmadiihaikal@gmail.com>
2021-10-11 03:02:15 +0700

    aw

---
+++ script.js
@@ -0,0 +1,76 @@
+var listmakanan =[];
+var makananInput = document.getElementById("makanan");
+var randomButton = document.getElementById("random");
+var refreshButton = document.getElementById("refresh");
+
+var hasil = document.getElementById("result");
+var tambahButton = document.getElementById("tambah");
+var hasilakhir = document.getElementById("hasil_akhir");
+function store()
+{   
+    var listmakananvalue =listmakanan.push(makananInput.value);
+    if(makananInput.value =="")
+    {
+        alert("Masukkin dulu dong");
+    }
+    else{
+        if (listmakananvalue >=10)
+        {
+            alert("kamu udah masukin makanan lebih dari " +listmakanan.length)
+            tambahButton.disabled = "true";
+        }
+        show();
+    }
+   
+}
+
+function hapus() {
+    makananInput.value= "";
+}
+
+function show(){
+    hasil.innerHTML = "";
+    if (makananInput != "")
+    {
+        hasil.innerHTML += "list kamu: " + "<br> <li> " + listmakanan.join(", ") + "</li> <br>";
+        var total = listmakanan.length
+        if(total >= 5)
+        {
+            randomButton.style.display = "block";
+            refreshButton.style.display = "block";
+
+        }
+    }
+   
+    console.log(listmakanan)
+   
+}
+
+function random() {
+    var randommakanan = listmakanan[Math.floor(Math.random()* listmakanan.length)];
+    console.log(randommakanan)
+    // hasilakhir.innerHTML= randommakanan;
+    Swal.fire({
+        title: 'Yakin tempat makannya itu aja?',
+        showCancelButton: true,
+        confirmButtonText: 'Iya yakin',
+        cancelButtonText: 'Hmmm',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("Selamat hari ini kamu makan "+randommakanan, '', 'success')
+            .then(function(isConfirm)
+            {
+                if(isConfirm){
+                    location.reload();
+                }
+            })
+
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+
+}
+function refresh() {
+    window.location.reload()
+}
